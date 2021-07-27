927e3947b804604b65a1ac6aefd430e0a23a6993
Hector Galvan <inghgalvan@outlook.com>
2021-07-26 19:27:34 -0500

    operaciones nuevo js

---
+++ codigo_facilito/nuevo_curso_profesional/2.-conceptos_basicos/2var_const.js
@@ -0,0 +1,17 @@
+/*
+una variable se declara con las palabras let o var
+
+let nombre_variable 
+
+
+una constante se declara con la palabra const
+
+nombre de las variables no pueden llevar espacios
+
+pueden empezar con una letra o $ o _
+
+*/
+
+let nombre = "Hèctor";
+
+const PI = 3.1416
---
+++ codigo_facilito/nuevo_curso_profesional/2.-conceptos_basicos/3operaciones_numeros.js
@@ -0,0 +1,30 @@
+/* 
+los numeros son de tipo number sin importar si son enteros o flotantes
+
+*/
+
+let edad = 31;
+
+let cambio = 2.5;
+
+suma = edad + cambio;
+
+resta = edad - cambio;
+
+multiplicacion = edad * cambio;
+
+division = edad / cambio;
+
+residuo = edad % cambio;
+
+//otra forma es con la libreria math
+
+pi = Math.PI;
+
+//potencia 10 al cuadrado
+
+potencia = Math.pow(10,2)
+
+//redondear
+
+redondeo = Math.round(6.6)
